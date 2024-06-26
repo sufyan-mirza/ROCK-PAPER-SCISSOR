bd292112d1edfcaa8a0073b590cd550f73933e2a
sufyan-mirza <160243704+sufyan-mirza@users.noreply.github.com>
2024-06-26 21:26:16 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// ROCK PAPER SCISSOR
+import inquirer from "inquirer";
+// person 1
+let person1 = await inquirer.prompt([
+    {
+        type: 'string',
+        name: 'choose',
+        message: 'select rock paper scissor',
+    }
+]);
+// person 2
+let person2 = await inquirer.prompt([
+    {
+        type: 'string',
+        name: 'choose',
+        message: 'select rock paper scissor',
+    }
+]);
+// we use if else statement
+if (person1.choose === person2.choose) {
+    console.log("game tied");
+}
+else if ((person1.choose === "paper" && person2.choose === "rock") || (person1.choose === "scissor" && person2 === "paper") ||
+    (person1 === "rock" && person2 === "scissor")) {
+    console.log("player 1 wins");
+}
+else {
+    console.log("player 2 wins");
+}
